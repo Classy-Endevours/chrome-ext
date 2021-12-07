75f1675c41fdd0ca904c27c9a42255e078c64495
Piyush Dubey <piyush115dubey@gmail.com>
2021-12-07 13:05:19 +0530

    added chartjs

--- js/load/chart.js
+++ js/load/chart.js
@@ -1,7 +1,15 @@
 const injectScript = () => {
+  const chartContainer = document.createElement("canvas");
+  chartContainer.setAttribute("id", "chartContainer");
+  document.body.appendChild(chartContainer);
+
   const script = document.createElement("script");
   script.src = "https://cdn.jsdelivr.net/npm/chart.js";
-  script.async = true;
   document.body.appendChild(script);
+  script.onload = () => {
+    const domScript = document.createElement("script");
+    domScript.src = chrome.extension.getURL("js/dom/chart.js");
+    document.body.appendChild(domScript);
+  };
 };
 injectScript();
